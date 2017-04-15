ac849018a4551debdabb213ca34e967ddd982f18
Butch <butchtabios@live.com>
2017-04-15 20:37:36 +0800

    Sync Display_nodeMCU_OLED to Client_ESP8266

--- Display_nodeMCU_OLED/webserver.ino
+++ Display_nodeMCU_OLED/webserver.ino
@@ -9,208 +9,273 @@
 #include <WiFiClient.h> 
 #include <ESP8266WebServer.h>
 
-// Access point credentials
-const char *ap_ssid = "ESPap";
-const char *ap_password = "thereisnospoon";
-
-// ESP8266 GPIO pins
-#define GPIO2 2
-
 ESP8266WebServer webserver(80);
-
-void (*DisplayLogo)(bool bLogo) = _DisplayLogo;
 
 void webserver_setup()
 {
-	/* Set page handler functions */
-	webserver.on("/", rootPageHandler);
-	webserver.on("/wlan_config", wlanPageHandler);
-	webserver.on("/gpio", gpioPageHandler);
-	webserver.onNotFound(handleNotFound);
+  /* Set page handler functions */
+  webserver.on("/", rootPageHandler);
+  webserver.on("/wlan_config", wlanPageHandler);
+  webserver.on("/server", serverPageHandler);
+  webserver.onNotFound(handleNotFound);
 
-	webserver.begin();
+  webserver.begin();
+
+  bWiFiConnect = true;
+  bServerConnect = false;
+  last_connect = millis();
+  interval = WIFI_CONNECT_INTERVAL;
+  Reconnect = 0;
 }
 
 inline void webserver_loop()
 {
-	webserver.handleClient();
+  webserver.handleClient();
 }
 
 /* Root page for the webserver */
 void rootPageHandler()
 {
-	String response_message = "<html><head><title>ESP8266 Webserver</title></head>";
-	response_message += "<body style=\"background-color:PaleGoldenRod\"><h1><center>ESP8266 Webserver</center></h1>";
+  String response_message = "<html><head><title>ShotClock Display Client</title></head>";
+  response_message += "<body style=\"background-color:PaleGoldenRod\"><h1><center>"+ap_ssid+" Display</center></h1>";
+  response_message += "<h2><center>Controller Client</center></h3>";
 
-	if (WiFi.status() == WL_CONNECTED)
-	{
-		response_message += "<h3><center>WLAN Status: Connected</center></h3>";
-	}
-	else
-	{
-		response_message += "<h3><center>WLAN Status: Disconnected</center></h3>";
-	}
+  if (WiFi.isConnected())
+  {
+    response_message += "<h3><center>WLAN Status: Connected</center></h3>";
+  }
+  else
+  {
+    response_message += "<h3><center>WLAN Status: Disconnected</center></h3>";
+  }
 
-	response_message += "<h4><center><a href=\"/wlan_config\">Configure WLAN settings</a></center></h4>";
-	response_message += "<h4><center><a href=\"/gpio\">Display Logo On/Off</h4></li></center></h4>";
-	response_message += "</body></html>";
+  response_message += "<h4><center><a href=\"/wlan_config\">Configure WLAN settings</a></center></h4>";
+  response_message += "<h4><center><a href=\"/server\">Configure Server settings</a></center></h4>";
+  response_message += "</body></html>";
 
-	webserver.send(200, "text/html", response_message);
+  webserver.send(200, "text/html", response_message);
 }
 
 /* WLAN page allows users to set the WiFi credentials */
 void wlanPageHandler()
 {
-	// Check if there are any GET parameters
-	if (webserver.hasArg("ssid"))
-	{
-		if (webserver.hasArg("password"))
-		{
-			WiFi.begin(webserver.arg("ssid").c_str(), webserver.arg("password").c_str());
-		}
-		else
-		{
-			WiFi.begin(webserver.arg("ssid").c_str());
-		}
+  // Check if there are any GET parameters
+  bool bChange = false;
+  if (webserver.hasArg("ssid"))
+  {
+    if (sta_ssid!=webserver.arg("ssid"))
+    {
+      sta_ssid = webserver.arg("ssid");
+      bChange = true;
+    }
+    if (webserver.hasArg("password"))
+    {
+      if (sta_passwd!=webserver.arg("password"))
+      {
+        sta_passwd = webserver.arg("password");
+        bChange = true;
+      }
+    }
+    else
+    {
+      if (sta_passwd.length()>0) 
+      {
+        sta_passwd = "";
+        bChange = true;
+      }
+    }
+    
+    if ((!bWiFiConnect&&!WiFi.isConnected())||bChange)
+    {
+      if (bServerConnect) serverConnect(false);
+      
+      if (sta_passwd.length()>0)
+      {
+        WiFi.begin(sta_ssid.c_str(), sta_passwd.c_str());
+      }
+      else
+      {
+        WiFi.begin(sta_ssid.c_str());
+      }
+      #ifdef DebugSerial
+      DebugSerial.print("Connecting to SSID:");
+      DebugSerial.print(sta_ssid);
+      DebugSerial.print(" Password:");
+      DebugSerial.println(sta_passwd);
+      #endif
+      webserver_setup();
+    }
+  }
 
-		String ReConnectStr("Reconnecting... ");
-		//ResetScrollPos();
-		//int Len = LoadMessage(ReConnectStr.c_str());
+  String response_message = "";
+  response_message += "<html>";
+  if (bWiFiConnect)
+  {
+    response_message += "<head><title>ShotClock Display Client</title>";
+    response_message += "<meta http-equiv=\"refresh\" content=\"10; url=/wlan_config\"></head>";
+  }
+  else
+  {
+    response_message += "<head><title>ShotClock Display Client</title></head>";
+  }
+  response_message += "<body style=\"background-color:PaleGoldenRod\"><h1><center>WLAN Settings</center></h1>";
+  
+  response_message += "<center><a href=\"/\">Return to main page</a></center><br>";
 
-		for (int i = 0; i<200; i++)
-		{
-			if (WiFi.status() == WL_CONNECTED)
-			{
-				Serial.println("WiFi reconnected");
-				Serial.println("New IP address: ");
-				Serial.println(WiFi.localIP());
-				break;
-			}
-			//LoadDisplayBuffer(Len);
-			delay(50);
-		}
-		//ResetScrollPos();
-		delay(1000);
-		webserver_setup();
-	}
+  if (WiFi.isConnected())
+  {
+    response_message += "<center>WiFi Status: Connected</center>";
+  }
+  else
+  {
+    if (bWiFiConnect)
+    {
+      response_message += "<center>WiFi Status: Connecting</center>";
+    }
+    else
+    {
+      response_message += "<center>WiFi Status: Disconnected</center>";
+    }
+  }
+  response_message += "<center>Local IP Address: "+WiFi.localIP().toString()+"</center>";
+  response_message += "<center>Subnet Mask: "+WiFi.subnetMask().toString()+"</center>";
+  response_message += "<center>GW IP Address: "+WiFi.gatewayIP().toString()+"</center><br>";
 
-	String response_message = "";
-	response_message += "<html>";
-	response_message += "<head><title>ESP8266 Webserver</title></head>";
-	response_message += "<body style=\"background-color:PaleGoldenRod\"><h1><center>WLAN Settings</center></h1>";
+  response_message += "<center><p>To connect to a WiFi network, please select a network...</p></center>";
 
-	response_message += "<ul><li><a href=\"/\">Return to main page</a></li></ul>";
+  // Get number of visible access points
+  int ap_count = WiFi.scanNetworks();
 
-	if (WiFi.status() == WL_CONNECTED)
-	{
-		response_message += "Status: Connected<br>";
-	}
-	else
-	{
-		response_message += "Status: Disconnected<br>";
-	}
+  if (ap_count == 0)
+  {
+    response_message += "<center>No access points found.</center><br>";
+  }
+  else
+  {
+    response_message += "<form method=\"get\">";
+    // Show access points
+    for (uint8_t ap_idx = 0; ap_idx < ap_count; ap_idx++)
+    {
+      if (String(WiFi.SSID(ap_idx))==sta_ssid)
+      {
+        response_message += "<center><input type=\"radio\" name=\"ssid\" value=\"" + String(WiFi.SSID(ap_idx)) + "\" checked>";
+      }
+      else
+      {
+        response_message += "<center><input type=\"radio\" name=\"ssid\" value=\"" + String(WiFi.SSID(ap_idx)) + "\">";
+      }
+      response_message += String(WiFi.SSID(ap_idx)) + " (RSSI: " + WiFi.RSSI(ap_idx) + ")";
+      (WiFi.encryptionType(ap_idx) == ENC_TYPE_NONE) ? response_message += " " : response_message += "*";
+      response_message += "</center><br>";
+    }
 
-	response_message += "<p>To connect to a WiFi network, please select a network...</p>";
+    response_message += "<center>WiFi password (if required):</center>";
+    response_message += "<center><input type=\"text\" value=\""+sta_passwd+"\" name=\"password\"></center><br>";
+    response_message += "<center><input type=\"submit\" value=\"Connect\"></center>";
+    response_message += "</form>";
+  }
+  
+  response_message += "</body></html>";
 
-	// Get number of visible access points
-	int ap_count = WiFi.scanNetworks();
-
-	if (ap_count == 0)
-	{
-		response_message += "No access points found.<br>";
-	}
-	else
-	{
-		response_message += "<form method=\"get\">";
-
-		// Show access points
-		for (uint8_t ap_idx = 0; ap_idx < ap_count; ap_idx++)
-		{
-			response_message += "<input type=\"radio\" name=\"ssid\" value=\"" + String(WiFi.SSID(ap_idx)) + "\">";
-			response_message += String(WiFi.SSID(ap_idx)) + " (RSSI: " + WiFi.RSSI(ap_idx) + ")";
-			(WiFi.encryptionType(ap_idx) == ENC_TYPE_NONE) ? response_message += " " : response_message += "*";
-			response_message += "<br><br>";
-		}
-
-		response_message += "WiFi password (if required):<br>";
-		response_message += "<input type=\"text\" name=\"password\"><br>";
-		response_message += "<input type=\"submit\" value=\"Connect\">";
-		response_message += "</form>";
-	}
-
-	response_message += "</body></html>";
-
-	webserver.send(200, "text/html", response_message);
-}
-
-boolean bLogo = true;
-
-void _DisplayLogo(bool bLogo)
-{
+  webserver.send(200, "text/html", response_message);
 }
 
 /*///////////////////////////////////////////////////////////////////////////*/
 
-/* GPIO page allows you to control the GPIO pins */
-void gpioPageHandler()
+void serverPageHandler()
 {
-	// Check if there are any GET parameters
-	if (webserver.hasArg("gpio2"))
-	{
-		if (webserver.arg("gpio2") == "1")
-		{
-			//digitalWrite(GPIO2, HIGH);
-			bLogo = true;
-		}
-		else
-		{
-			//digitalWrite(GPIO2, LOW);
-			bLogo = false;
-		}
-    DisplayLogo(bLogo);
-	}
+  // Check if there are any GET parameters
+  if (webserver.hasArg("serverIP"))
+  {
+    serverIP.fromString(webserver.arg("serverIP").c_str());
+  }
 
-	String response_message = "<html><head><title>ESP8266 Webserver</title></head>";
-	response_message += "<body style=\"background-color:PaleGoldenRod\"><h1><center>Control GPIO pins</center></h1>";
-	response_message += "<form method=\"get\">";
+  if (webserver.hasArg("portno"))
+  {
+    localPort = (uint16_t)webserver.arg("portno").toInt();
+  }
+  
+  if (webserver.hasArg("action"))
+  {
+    if (webserver.arg("action") == "Connect")
+    {
+      serverConnect(true);
+      Reconnect = 0;
+    }
+    else
+    {
+      serverConnect(false);
+    }
+  }
 
-	response_message += "<ul><li><a href=\"/\">Return to main page</a></li></ul>";
+  String response_message = "<html><head><title>ShotClock Display Client</title></head>";
+  response_message += "<body style=\"background-color:PaleGoldenRod\"><h1><center>Configure Server</center></h1>";
 
-	response_message += "LOGO:<br>";
+  response_message += "<center><a href=\"/\">Return to main page</a></center><br>";
 
-	if (bLogo == false)
-	{
-		response_message += "<input type=\"radio\" name=\"gpio2\" value=\"1\" onclick=\"submit();\">On<br>";
-		response_message += "<input type=\"radio\" name=\"gpio2\" value=\"0\" onclick=\"submit();\" checked>Off<br>";
-	}
-	else
-	{
-		response_message += "<input type=\"radio\" name=\"gpio2\" value=\"1\" onclick=\"submit();\" checked>On<br>";
-		response_message += "<input type=\"radio\" name=\"gpio2\" value=\"0\" onclick=\"submit();\">Off<br>";
-	}
+  bool bConnected;
+  
+  if (wifiClient.connected())
+  {
+    response_message += "<center>Status: Connected to server</center>";
+    bConnected = true;
+  }
+  else
+  {
+    response_message += "<center>Status: Disconnected from server</center>";
+    bConnected = false;
+  }
+  response_message += "<center>Local IP Address: "+WiFi.localIP().toString()+"</center>";
+  response_message += "<center>Subnet Mask: "+WiFi.subnetMask().toString()+"</center>";
+  response_message += "<center>GW IP Address: "+WiFi.gatewayIP().toString()+"</center><br>";
 
-	response_message += "</form></body></html>";
+  response_message += "<form method=\"get\">";
+  
+  response_message += "<center>Server Address</center>";
+  response_message += "<center><input type=\"text\" value=\""+serverIP.toString()+"\" name=\"serverIP\"></center><br>";
+  
+  response_message += "<center>Port No.</center>";
+  response_message += "<center><input type=\"text\" value=\""+String(localPort)+"\" name=\"portno\"></center><br>";
+  
+  if (bConnected == false)
+  {
+    if (bWiFiConnect)
+    {
+      response_message += "<center><input type=\"submit\" name=\"action\" value=\"Connecting\"></center>";
+    }
+    else
+    {
+      response_message += "<center><input type=\"submit\" name=\"action\" value=\"Connect\"></center>";
+    }
+  }
+  else
+  {
+    response_message += "<center><input type=\"submit\" name=\"action\" value=\"Disconnect\"></center>";
+  }
+  response_message += "</form>";
+  
+  response_message += "</body></html>";
 
-	webserver.send(200, "text/html", response_message);
+  webserver.send(200, "text/html", response_message);
 }
 
 /* Called if requested page is not found */
 void handleNotFound()
 {
-	String message = "File Not Found\n\n";
-	message += "URI: ";
-	message += webserver.uri();
-	message += "\nMethod: ";
-	message += (webserver.method() == HTTP_GET) ? "GET" : "POST";
-	message += "\nArguments: ";
-	message += webserver.args();
-	message += "\n";
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += webserver.uri();
+  message += "\nMethod: ";
+  message += (webserver.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += webserver.args();
+  message += "\n";
 
-	for (uint8_t i = 0; i < webserver.args(); i++)
-	{
-		message += " " + webserver.argName(i) + ": " + webserver.arg(i) + "\n";
-	}
+  for (uint8_t i = 0; i < webserver.args(); i++)
+  {
+    message += " " + webserver.argName(i) + ": " + webserver.arg(i) + "\n";
+  }
 
-	webserver.send(404, "text/plain", message);
+  webserver.send(404, "text/plain", message);
 }
 
